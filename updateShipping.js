2f3674d02a8e465c9aa772b0d0a1b4592acf3096
Michael Peterson <WebDev@ctoms.ca>
2019-05-21 11:50:03 -0600

    first version

--- updateShipping.js
+++ updateShipping.js
@@ -32,10 +32,59 @@ UpdateShip.prototype.updatePrice = function(){
 
 UpdateShip.prototype.radioClicked = function(event){
 	
-	let value = event.currentTarget.value;
+	//let value = event.currentTarget.value;
 	//split by "long" dash
 	let carrier = event.currentTarget.nextSibling.textContent.split("–")[0].trim();
-	console.log("clicked", carrier, value);
+	let price = event.currentTarget.nextSibling.textContent.split("–")[1].trim().replace("$","");
+	let rateSystem;
+	if(carrier.search(/ups/gi) !== -1){
+		rateSystem = "ups";
+	}
+	else if(carrier.search(/fedex/gi) !== -1){
+		rateSystem = "fedex";
+	}
+	else{
+		rateSystem = "other";
+	}
+	console.log("clicked", carrier, price,rateSystem);
+	this.setShip(price,carrier,rateSystem);
 }
 
-let updateShip = new UpdateShip("sShipMeth");
+UpdateShip.prototype.setShip = function(amount,desc,ratesystem)
+{
+  
+	try{
+
+		$('#custbody_newship').val(amount);
+		$('#custbody_carrier').val(desc); 
+		$('#custbody_ratesystem').val(ratesystem); 
+		$('input[name=shiptotal]').val(amount);
+		 
+
+		$('#ordersummary_shipping td:contains("$")').html("$"+amount);
+		var ship = $('#ordersummary_shipping td:contains("$")').html();
+		if(ship.charAt(0) == '$')
+			ship = ship.substring(1,ship.length-0);
+		var totalSO =   Number(ship)  + Number(i_itemtot)  + Number(i_tax);
+		$('#ordersummary_total td:contains("$")').html("$"+parseFloat(totalSO).toFixed(2));
+		 
+
+		$('input[name=shiptotal]').val(amount); 
+		//debugger;
+	}
+	catch(er) {
+       
+        var text = "Error description: " + er.message + "\n\n";
+        console.log(text);
+       
+    }
+ 
+}
+try{
+	let updateShip = new UpdateShip("sShipMeth");
+}
+
+catch(er){
+	console.log("error: ",er);
+}
+
