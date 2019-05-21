6fb276f93cec23fac65cb7daa481336f809cb3d6
Michael Peterson <WebDev@ctoms.ca>
2019-05-21 10:26:11 -0600

    added element for testing

--- updateShipping.js
+++ updateShipping.js
@@ -1,10 +1,19 @@
 function UpdateShip(shipButtonName){
 	this.shippingButtons = $("input[name*=" + shipButtonName +"]");
 	console.log(this.shippingButtons);
+	this.initButtons();
 }
 
 UpdateShip.prototype.initButtons = function(){
-	
+	/*
+	for(let i = 0;i < this.shippingButtons.length; i++){
+		console.log("button text: ",this.shippingButtons[i].nextSibling.textContent);
+	}
+	*/
+	this.shippingButtons[0].onclick = null;
+	this.shippingButtons[0].addEventListener("click",function(e){
+		console.log("clicked");
+	}.bind(this));
 }
 
 let updateShip = new UpdateShip("sShipMeth");
