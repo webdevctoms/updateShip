cb0ccefd599461a6868302c35579fbd0e2436e6f
Michael Peterson <WebDev@ctoms.ca>
2019-05-21 10:49:35 -0600

    added update price

--- updateShipping.js
+++ updateShipping.js
@@ -2,7 +2,9 @@ function UpdateShip(shipButtonName){
 	this.shippingButtons = $("input[name*=" + shipButtonName +"]");
 	this.upShip = 8;
 	console.log(this.shippingButtons);
+	this.updatePrice();
 	this.initButtons();
+
 }
 
 UpdateShip.prototype.initButtons = function(){
@@ -12,6 +14,19 @@ UpdateShip.prototype.initButtons = function(){
 		this.shippingButtons[i].addEventListener("click",function(e){		
 			this.radioClicked(e);
 		}.bind(this));
+	}
+}
+
+UpdateShip.prototype.updatePrice = function(){
+	
+	for(let i = 0;i < this.shippingButtons.length; i++){
+		let splitString = this.shippingButtons[i].nextSibling.textContent.split("–");
+		let currentVal = parseFloat(splitString[1].trim().replace("$",""));
+		if(currentVal){
+			let newVal = currentVal + this.upShip;
+			let newString = splitString[0] + "– $" + newVal;
+			this.shippingButtons[i].nextSibling.textContent = newString;
+		}
 	}
 }
 
